e748b0668ef84513a261bcd0d7f30e82736fd7e6
Guilherme Mendes <boralagui@outlook.com>
2022-03-06 07:06:03 -0300

    Commitando projeto front e backend

---
+++ GSPro-API/src/main/java/br/com/GSProAPI/GsProApiApplication.java
@@ -0,0 +1,13 @@
+package br.com.GSProAPI;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GsProApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GsProApiApplication.class, args);
+	}
+
+}
---
+++ GSPro-API/src/test/java/br/com/GSProAPI/GsProApiApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.GSProAPI;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GsProApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
